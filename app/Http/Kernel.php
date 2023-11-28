d22300ea3233e539ca593d2010b3c8cf78900875
Ariq Khoiri <qq.khoiri@gmail.com>
2023-11-28 15:12:36 +0700

    feat fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,9 +15,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
-        //\Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
