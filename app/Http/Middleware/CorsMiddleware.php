366fba2b12011f5c4becbf359716c385b6480750
Ariq Khoiri <qq.khoiri@gmail.com>
2023-11-30 13:03:07 +0700

    fix cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use http\Client\Response;
 use Illuminate\Http\Request;
 
 class CorsMiddleware
@@ -17,21 +16,8 @@ class CorsMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
-
-
-        $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-        ];
-        if($request->getMethod() == "OPTIONS") {
-            return Response::make('OK', 200, $headers);
-        }
-
-
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
